f0b7e43fba1fb85a132e8eb8e12633b64f98bcd0
PKR24 <101044221+PKR24@users.noreply.github.com>
2022-03-30 10:45:42 +0530

    Update smr.c

--- sample_implementation/smr.c
+++ sample_implementation/smr.c
@@ -4,11 +4,9 @@
 #include"menu.h"
 int main(int argc,char **argv)
 {
-    //gotoxy(15,8);
     printf("<--:Student Record Management System:-->\n");
     printf("Press any key to continue.\n");
-    gotoxy(19,15);
-    getch();
+    getchar();
     menu();
     return 0;
 }
